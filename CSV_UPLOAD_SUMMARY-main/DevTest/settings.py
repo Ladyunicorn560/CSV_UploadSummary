70ffb92151a92b8c6e132aa2e516927f8fa3fd58
Shiksha Shrivastava <132224370+Ladyunicorn560@users.noreply.github.com>
2024-10-15 15:46:07 +0530

    Update settings.py

--- CSV_UPLOAD_SUMMARY-main/DevTest/settings.py
+++ CSV_UPLOAD_SUMMARY-main/DevTest/settings.py
@@ -128,7 +128,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shrivastavashiksha03@gmail.com'
-EMAIL_HOST_PASSWORD = 'Shi@miraculous'
+EMAIL_HOST_USER = 'mail'
+EMAIL_HOST_PASSWORD = 'pswrd'
 
 logging.basicConfig(level=logging.DEBUG)
